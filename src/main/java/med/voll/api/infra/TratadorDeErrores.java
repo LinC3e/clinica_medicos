1413934032a97a580419da1817c5f8cb63d3b1a0
LinC3e <ivancostilla17@gmail.com>
2023-05-28 16:08:10 -0300

    exception handler error 400 ,validation

--- src/main/java/med/voll/api/infra/TratadorDeErrores.java
+++ src/main/java/med/voll/api/infra/TratadorDeErrores.java
@@ -2,6 +2,8 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,5 +12,17 @@ public class TratadorDeErrores {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarError404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarError400(MethodArgumentNotValidException e) {
+        var errors = e.getFieldErrors().stream().map(DatosErrorValidacion::new).toList();
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    private record DatosErrorValidacion(String campo , String error) {
+        public DatosErrorValidacion(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
