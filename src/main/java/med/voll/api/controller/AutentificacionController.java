f18b28cd06ec800d8a445e200aa46a5a945039c1
LinC3e <ivancostilla17@gmail.com>
2023-05-29 15:33:26 -0300

    return users mysql

--- src/main/java/med/voll/api/controller/AutentificacionController.java
+++ src/main/java/med/voll/api/controller/AutentificacionController.java
@@ -1,6 +1,7 @@
 package med.voll.api.controller;
 
 
+import jakarta.validation.Valid;
 import med.voll.api.domain.usuarios.DatosAutentificacionUsuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +21,7 @@ public class AutentificacionController {
     private AuthenticationManager authenticationManager;
 
     @PostMapping
-    public ResponseEntity auntenticarUsuario(DatosAutentificacionUsuario datosAutentificacionUsuario){
+    public ResponseEntity auntenticarUsuario(@RequestBody @Valid DatosAutentificacionUsuario datosAutentificacionUsuario){
         Authentication token = new UsernamePasswordAuthenticationToken(datosAutentificacionUsuario.login(), datosAutentificacionUsuario.clave());
         authenticationManager.authenticate(token);
         return ResponseEntity.ok().build();
